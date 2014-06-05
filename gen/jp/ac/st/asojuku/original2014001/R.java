7a6f6e4b694af898080ad81f09d26f15bc320d1d
student <student@PC633S17.fgakusei.local>
2014-06-05 11:03:38 +0900

    警告消しました

--- gen/jp/ac/st/asojuku/original2014001/R.java
+++ gen/jp/ac/st/asojuku/original2014001/R.java
@@ -59,7 +59,22 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int chukiti=0x7f050007;
+        public static final int daikiti=0x7f050006;
+        public static final int daikyou=0x7f050005;
+        public static final int gatyadora=0x7f05000c;
+        public static final int gofes=0x7f05000d;
+        public static final int golem=0x7f05000f;
         public static final int hello_world=0x7f050002;
+        public static final int komei=0x7f050010;
+        public static final int kuji=0x7f050009;
+        public static final int kyou=0x7f050004;
+        public static final int largetext=0x7f050008;
+        public static final int nyuryoku=0x7f05000a;
+        public static final int ok=0x7f05000b;
+        public static final int seiren=0x7f05000e;
+        public static final int sonken=0x7f050011;
+        public static final int unsei=0x7f050003;
     }
     public static final class style {
         /** 
